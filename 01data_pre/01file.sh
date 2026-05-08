345da71181f394471a899e1857245f18fd16806a
Chenjin-l <lingchenjin@outlook.com>
2026-05-08 22:31:57 +0800

    Update 01file.sh

--- 01data_pre/01file.sh
+++ 01data_pre/01file.sh
@@ -1,3 +1,5 @@
+##转为11.1的位点
+###############
 awk 'NR>1 {print $2"\t"$7"\t"$8}' JXLab_ChinaChip50K_PLUS_SSC11.map > map11.txt
 awk '
 BEGIN{
@@ -20,9 +22,7 @@ NR==FNR{
 }
 ' map11.txt EE.map > EE.SSC11.map
 
-##path=/work/lingcj/03chip_data/01_data/01check
-
-
+###保留常染色体，提取需要保留的样本id
 plink --file /work/lingcj/03chip_data/01_data/01check/data/EE.SSC11 \
       --chr 1-18 \
       --keep /work/lingcj/03chip_data/02anlyis/01data/01pre_exchange/ID_compare/growth_keep.txt \
@@ -30,7 +30,7 @@ plink --file /work/lingcj/03chip_data/01_data/01check/data/EE.SSC11 \
       --out EE_growth
 
 # =========================
-# 1. 基础QC（芯片数据）
+# 1. 基础QC
 # =========================
 
 plink --bfile EE_growth \
@@ -40,13 +40,7 @@ plink --bfile EE_growth \
   --make-bed \
   --out EE_QC_growth
 
-mkdir -p PCA
-plink --bfile EE_QC_growth --pca 20 --out PCA/EE_growth_pca
-
-source ~/anaconda3/etc/profile.d/conda.sh                                                                                               
-conda activate R4 
-Rscript pca_plot.r > plot.log
-
+###转为vcf，改id
 plink --bfile ../EE_QC_growth --recode vcf-iid --out EE_vcf
 
 bcftools annotate \
