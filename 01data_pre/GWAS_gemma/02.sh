abf73f762e6765042e6aef32fdb0c7b4b0586c6e
Chenjin-l <lingchenjin@outlook.com>
2026-05-27 16:25:34 +0800

    Update 02.sh

--- 01data_pre/GWAS_gemma/02.sh
+++ 01data_pre/GWAS_gemma/02.sh
@@ -69,3 +69,82 @@ gemma \
 -o chr${chr}_${i}
 
 echo "Finished chr${chr}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+#PBS-N gemma_merge
+#PBS-q batch
+#PBS-l nodes=1:ppn=4
+#PBS-l mem=10G
+#PBS-o /home/Mzhou/zlingcj/log
+#PBS-e /home/Mzhou/zlingcj/log
+#PBS-j oe
+#PBS-t 3-5
+set -euo pipefail
+
+echo "Start: $(date)"
+
+########################################
+# 表型编号（手动修改）
+########################################
+i=${PBS_ARRAYID}
+
+INDEX=/home/Mzhou/zlingcj/03chip_data/02anlyis/01data/03growth/03GWAS/02imputation/02_1kpig_imp_gwas/01data/02pheno_covar/index_growth.txt
+
+name=$(awk -v n=$i 'NR==n{print $1}' ${INDEX})
+
+echo "Trait ID : ${i}"
+echo "Trait name: ${name}"
+
+########################################
+# 路径
+########################################
+BASE=/home/Mzhou/zlingcj/03chip_data/02anlyis/04swim/03sexgwas/03gemma/03result/male
+
+WORKDIR=${BASE}/${i}_${name}
+OUTDIR=${WORKDIR}/output
+
+cd ${WORKDIR}
+
+########################################
+# 合并 chr1-18
+########################################
+echo "Merging..."
+
+cat ${OUTDIR}/chr*_${i}.assoc.txt \
+| grep -v "^#" \
+| sort -gk 12,12 -S 20% --parallel=30 \
+> ${name}_${i}.assoc.txt
+
+sed -i '1,17d' ${name}_${i}.assoc.txt
+echo "Done: ${name}_${i}.assoc.txt"
+echo "End: $(date)"
+02merge.sh (END)
+
+
+
+
+
+
+
